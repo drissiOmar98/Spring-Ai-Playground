1cfea2b9a81108a6b2f2a16870fea9eb217bb0a7
DrissiOmar <omar.drissi@esprit.tn>
2026-08-18 23:03:22 +0100

    feat(chatclient-vs-chatmodel): initialize Spring AI project to explore ChatClient and ChatModel APIs

---
+++ chatclient-vs-chatmodel/src/main/java/com/omar/chatclient_vs_chatmodel/ChatclientVsChatmodelApplication.java
@@ -0,0 +1,13 @@
+package com.omar.chatclient_vs_chatmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatclientVsChatmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatclientVsChatmodelApplication.class, args);
+	}
+
+}
---
+++ chatclient-vs-chatmodel/src/test/java/com/omar/chatclient_vs_chatmodel/ChatclientVsChatmodelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.chatclient_vs_chatmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatclientVsChatmodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
