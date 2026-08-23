ec785c91a84f90736af915f708f2f02204a9c0ce
DrissiOmar <omar.drissi@esprit.tn>
2026-08-23 20:54:24 +0100

    init: initialize Spring Boot & Spring AI 2.0 real-time tool execution with real-time SSE streaming

---
+++ tool-calling-streaming/src/test/java/com/omar/tool_calling_streaming/ToolCallingStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.tool_calling_streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolCallingStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
