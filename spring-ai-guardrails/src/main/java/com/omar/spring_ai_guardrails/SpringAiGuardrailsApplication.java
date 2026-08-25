325bfef0992e155fb2766084ef3ea34711e7b4d3
DrissiOmar <omar.drissi@esprit.tn>
2026-08-25 18:44:59 +0100

    feat(guardrails): initialize project for multi-layered Spring AI data protection and guardrail advisor demos

---
+++ spring-ai-guardrails/src/main/java/com/omar/spring_ai_guardrails/SpringAiGuardrailsApplication.java
@@ -0,0 +1,13 @@
+package com.omar.spring_ai_guardrails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiGuardrailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiGuardrailsApplication.class, args);
+	}
+
+}
---
+++ spring-ai-guardrails/src/test/java/com/omar/spring_ai_guardrails/SpringAiGuardrailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.spring_ai_guardrails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiGuardrailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
